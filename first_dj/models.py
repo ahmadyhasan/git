986ed0030f86451f033b4cbf156ec9f855e3cc76
ehsan <1374ea@gmail.com>
2022-12-23 15:20:17 +0330

    Add category table

--- first_dj/models.py
+++ first_dj/models.py
@@ -1,7 +1,16 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+
+User = get_user_model()
+
+
+class Category(models.Model):
+    title = models.CharField('Title', max_length=64)
 
 
 class Article(models.Model):
     title = models.CharField('Title', max_length=128)
     is_published = models.BooleanField('Is Published', default=True)
     content = models.TextField('Content')
+    author = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
+    categories = models.ManyToManyField(Category)
--- first_dj/views.py
+++ first_dj/views.py
@@ -7,5 +7,5 @@ from first_dj.models import Article
 class Home(View):
     @staticmethod
     def get(request):
-        articles = Article.objects.all()
+        articles = Article.objects.select_related('author').prefetch_related('categories').filter(is_published=True)
         return render(request, 'home.html', {"articles": articles})
